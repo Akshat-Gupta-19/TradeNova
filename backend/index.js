54c67915deece319ec209841957de658f8a116c7
Akshat-Gupta-19 <akshatgupta@Akshats-MacBook-Air.local>
2026-07-31 16:15:59 +0530

    Fix CORS middleware

--- backend/index.js
+++ backend/index.js
@@ -18,15 +18,15 @@ const uri = process.env.MONGO_URL;
 
 const app = express();
 
-app.use(
-  app.use(cors({
-  origin: [
-    "https://tradenova-frontend-5aa8.onrender.com",
-    "https://tradenova-dashboard-wsw7.onrender.com"
-  ],
-  credentials: true
-  }))
-);
+
+app.use(cors({
+origin: [
+  "https://tradenova-frontend-5aa8.onrender.com",
+  "https://tradenova-dashboard-wsw7.onrender.com"
+],
+credentials: true
+}))
+
 
 app.use(bodyParser.json());
 app.use(cookieParser());
